b318cfbe20dfbb448bd98f1bc89ef95ce6b2c9e7
Marcos Piccinini <x@nofxx.com>
2015-07-19 04:55:41 -0300

    let rubocop be happy in spec/

--- spec/mongoid/enum_spec.rb
+++ spec/mongoid/enum_spec.rb
@@ -5,77 +5,79 @@ class User
   include Mongoid::Enum
 
   enum :status, [:awaiting_approval, :approved, :banned]
-  enum :roles, [:author, :editor, :admin], :multiple => true, :default => [], :required => false
+  enum :roles, [:author, :editor, :admin], multiple: true, default: [], required: false
 end
 
 describe Mongoid::Enum do
   let(:klass) { User }
   let(:instance) { User.new }
   let(:alias_name) { :status }
-  let(:field_name) { :"#{alias_name}" }
+  let(:field_name) { alias_name.to_sym }
   let(:values) { [:awaiting_approval, :approved, :banned] }
   let(:multiple_field_name) { :roles }
 
-  describe "field" do
-    it "is defined" do
+  describe 'field' do
+    it 'is defined' do
       expect(klass).to have_field(field_name)
     end
 
-    it "is aliased" do
+    it 'is aliased' do
       expect(instance).to respond_to alias_name
       expect(instance).to respond_to :"#{alias_name}="
       expect(instance).to respond_to :"#{alias_name}?"
     end
 
-    describe "type" do
-      context "when multiple" do
-        it "is an array" do
+    describe 'type' do
+      context 'when multiple' do
+        it 'is an array' do
           expect(klass).to have_field(multiple_field_name).of_type(Array)
         end
 
-        it "validates using a custom validator" do
-          expect(klass).to custom_validate(multiple_field_name).with_validator(Mongoid::Enum::Validators::MultipleValidator)
+        it 'validates using a custom validator' do
+          expect(klass)
+            .to custom_validate(multiple_field_name)
+            .with_validator(Mongoid::Enum::Validators::MultipleValidator)
         end
       end
 
-      context "when not multiple" do
-        it "is a symbol" do
+      context 'when not multiple' do
+        it 'is a symbol' do
           expect(klass).to have_field(field_name).of_type(Symbol)
         end
 
-        it "validates inclusion in values" do
+        it 'validates inclusion in values' do
           expect(klass).to validate_inclusion_of(field_name).to_allow(values)
         end
       end
     end
   end
 
-  describe "constant" do
-    it "is set to the values" do
+  describe 'constant' do
+    it 'is set to the values' do
       expect(klass::STATUS).to eq values
     end
   end
 
-  describe "accessors"do
-    context "when singular" do
-      describe "{{value}}!" do
-        it "sets the value" do
+  describe 'accessors'do
+    context 'when singular' do
+      describe '{{value}}!' do
+        it 'sets the value' do
           instance.save
           instance.banned!
           expect(instance.status).to eq :banned
         end
       end
 
-      describe "{{value}}?" do
-        context "when {{enum}} == {{value}}" do
-          it "returns true" do
+      describe '{{value}}?' do
+        context 'when {{enum}} == {{value}}' do
+          it 'returns true' do
             instance.save
             instance.banned!
             expect(instance.banned?).to eq true
           end
         end
-        context "when {{enum}} != {{value}}" do
-          it "returns false" do
+        context 'when {{enum}} != {{value}}' do
+          it 'returns false' do
             instance.save
             instance.banned!
             expect(instance.approved?).to eq false
@@ -84,10 +86,10 @@ describe Mongoid::Enum do
       end
     end
 
-    context "when multiple" do
-      describe "{{value}}!" do
-        context "when field is nil" do
-          it "creates an array containing the value" do
+    context 'when multiple' do
+      describe '{{value}}!' do
+        context 'when field is nil' do
+          it 'creates an array containing the value' do
             instance.roles = nil
             instance.save
             instance.author!
@@ -95,8 +97,8 @@ describe Mongoid::Enum do
           end
         end
 
-        context "when field is not nil" do
-          it "appends the value" do
+        context 'when field is not nil' do
+          it 'appends the value' do
             instance.save
             instance.author!
             instance.editor!
@@ -105,9 +107,9 @@ describe Mongoid::Enum do
         end
       end
 
-      describe "{{value}}?" do
-        context "when {{enum}} contains {{value}}" do
-          it "returns true" do
+      describe '{{value}}?' do
+        context 'when {{enum}} contains {{value}}' do
+          it 'returns true' do
             instance.save
             instance.author!
             instance.editor!
@@ -116,8 +118,8 @@ describe Mongoid::Enum do
           end
         end
 
-        context "when {{enum}} does not contain {{value}}" do
-          it "returns false" do
+        context 'when {{enum}} does not contain {{value}}' do
+          it 'returns false' do
             instance.save
             expect(instance.author?).to be false
           end
@@ -126,18 +128,18 @@ describe Mongoid::Enum do
     end
   end
 
-  describe "scopes" do
-    context "when singular" do
-      it "returns the corresponding documents" do
+  describe 'scopes' do
+    context 'when singular' do
+      it 'returns the corresponding documents' do
         instance.save
         instance.banned!
         expect(User.banned.to_a).to eq [instance]
       end
     end
 
-    context "when multiple" do
-      context "and only one document" do
-        it "returns that document" do
+    context 'when multiple' do
+      context 'and only one document' do
+        it 'returns that document' do
           instance.save
           instance.author!
           instance.editor!
@@ -145,8 +147,8 @@ describe Mongoid::Enum do
         end
       end
 
-      context "and more than one document" do
-        it "returns all documents with those values" do
+      context 'and more than one document' do
+        it 'returns all documents with those values' do
           instance.save
           instance.author!
           instance.editor!
@@ -159,29 +161,30 @@ describe Mongoid::Enum do
     end
   end
 
-  describe "default values" do
-    context "when not specified" do
-      it "uses the first value" do
+  describe 'default values' do
+    context 'when not specified' do
+      it 'uses the first value' do
         instance.save
         expect(instance.status).to eq values.first
       end
     end
 
-    context "when specified" do
-      it "uses the specified value" do
+    context 'when specified' do
+      it 'uses the specified value' do
         instance.save
         expect(instance.roles).to eq []
       end
     end
   end
 
-  describe "i18n" do
-    it "should have a helper to translate the enum" do
+  describe 'i18n' do
+    it 'should have a helper to translate the enum' do
       expect(instance.status_i18n).to eq('Awaiting Approval')
     end
 
-    it "should have a helper to translate the values" do
-      expect(instance.status_values).to eq([["Awaiting Approval", :awaiting_approval]])
+    it 'should have a helper to translate the values' do
+      expect(instance.status_values)
+        .to eq([['Awaiting Approval', :awaiting_approval]])
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
-$: << File.expand_path("../../lib", __FILE__)
+$LOAD_PATH << File.expand_path('../../lib', __FILE__)
 
 require 'database_cleaner'
 require 'mongoid'
 require 'mongoid-rspec'
 require 'mongoid/enum'
 
-ENV['MONGOID_ENV'] = "test"
+ENV['MONGOID_ENV'] = 'test'
 
 RSpec.configure do |config|
   config.include Mongoid::Matchers
@@ -23,4 +23,4 @@ I18n.default_locale = :en
 
 Mongo::Logger.logger.level = Logger::INFO if Mongoid::VERSION >= '5'
 
-Mongoid.load!(File.expand_path("../support/mongoid.yml", __FILE__), :test)
+Mongoid.load!(File.expand_path('../support/mongoid.yml', __FILE__), :test)
